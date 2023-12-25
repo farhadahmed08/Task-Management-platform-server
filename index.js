39436da9d85aceddcd6d487f7ac5724326c16c98
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-26 00:58:06 +0800

    hosting done

--- index.js
+++ index.js
@@ -5,23 +5,31 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient, ObjectId ,ServerApiVersion} = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb://0.0.0.0:27017/";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qaohrfy.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb://0.0.0.0:27017/";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("taskManagementDb").collection("users");
     const taskCollection = client.db("taskManagementDb").collection("task");
@@ -121,8 +129,8 @@ app.delete("/task/:id", verifyToken, async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
